74d53899000c81a56cebce6201f5fff955469873
Prashant Mane <prashantmn47@gmail.com>
2024-10-11 00:01:48 +0530

    padding added to login page

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import img from "../assets/Sign In.png";
 import Image from "next/image";
+import Link from "next/link";
 const Signin = () => {
   return (
     <>
@@ -11,52 +12,52 @@ const Signin = () => {
           <link rel="canonical" href="https://nigussystems.com/" />
         </Helmet> */}
         <aside className="h-[100%] w-full md:w-[45%]">
-          <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-              {/* <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/> */}
-              <h2 class="mt-10 headingfont text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              {/* <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/> */}
+              <h2 className="mt-10 headingfont text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                 Sign in to your account
               </h2>
             </div>
 
-            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm p-3 md:p-0">
-              <form class="space-y-6" action="#" method="POST">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm p-3 md:p-0">
+              <form className="space-y-6" action="#" method="POST">
                 <div>
                   <label
                     for="email"
-                    class="block headingfont text-sm font-medium leading-6 text-gray-900"
+                    className="block headingfont text-sm font-medium leading-6 text-gray-900"
                   >
                     Email address
                   </label>
-                  <div class="mt-2">
+                  <div className="mt-2">
                     <input
                       id="email"
                       name="email"
                       type="email"
                       autocomplete="email"
                       required
-                      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
                     />
                   </div>
                 </div>
 
                 <div>
-                  <div class="flex items-center justify-between">
+                  <div className="flex items-center justify-between">
                     <label
                       for="password"
-                      class="block headingfont text-sm font-medium leading-6 text-gray-900"
+                      className="block headingfont text-sm font-medium leading-6 text-gray-900"
                     >
                       Password
                     </label>
                   </div>
-                  <div class="mt-2">
+                  <div className="mt-2">
                     <input
                       id="password"
                       name="password"
                       type="password"
                       autocomplete="current-password"
                       required
-                      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
                     />
                   </div>
                 </div>
@@ -64,28 +65,28 @@ const Signin = () => {
                 <div>
                   <button
                     type="submit"
-                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 pfont"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 pfont"
                   >
                     Sign in
                   </button>
                 </div>
               </form>
-              <div class="text-sm text-center mt-6">
+              <div className="text-sm text-center mt-6">
                 <a
                   href="#"
-                  class="font-semibold text-indigo-600 hover:text-indigo-500 headingfont"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500 headingfont"
                 >
                   Forgot password?
                 </a>
               </div>
-              <p class="mt-6 text-center headingfont text-sm text-gray-500">
+              <p className="mt-6 text-center headingfont text-sm text-gray-500">
                 Not a member?
-                <a
-                  href="#"
-                  class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                <Link
+                  href="/sign-up"
+                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                 >
                   Start a 10 days free trial
-                </a>
+                </Link>
               </p>
             </div>
           </div>
