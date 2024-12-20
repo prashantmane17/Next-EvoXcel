72e60a80f7b0b8332a8b5fae7a28f084d0c36871
prashantmane17 <prashantmn47@gmail.com>
2024-12-20 18:12:24 +0530

    error sloved

--- src/app/components/NewNav.jsx
+++ src/app/components/NewNav.jsx
@@ -5,7 +5,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Link from "next/link";
 import Image from "next/image";
-import "./chatbot.css"
+import "./chat_Bot.css"
 
 const NewNav = () => {
   const [scrolled, setScrolled] = useState(false);
